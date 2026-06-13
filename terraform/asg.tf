5ad7a8f3ed9f9798509a11bd369d3c0370751fbf
himanshu <himanshu@debian.himanshu>
2026-06-13 22:07:44 +0530

    update

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -52,12 +52,12 @@ resource "aws_security_group" "asg_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   from_port   = 0
-  #   to_port     = 65535
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # Allow inbound ICMP for Ping
   ingress {   
