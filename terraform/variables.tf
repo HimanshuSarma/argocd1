e045440822c8da7a1898c08e9b9265cabb2ae960
himanshu <himanshu@debian.himanshu>
2026-06-18 10:35:44 +0530

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "vpc_name" {
   description = "VPC name"
   type = string
-  default = "1-mini-project"
+  default = "project1"
 }
 
 variable "vpc_cidr_block" {
@@ -29,7 +29,7 @@ variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
   type = list(string)
   # default = ["10.0.101.0/24", "10.0.102.0/24"]
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24"]
 }
 
 variable "vpc_create_database_subnet_group" {
