faf9032f8ab0bdcf8778872fda96ea7c30161710
himanshu <himanshu@debian.himanshu>
2026-06-22 16:23:15 +0530

    update

--- microservices/auth/index.js
+++ microservices/auth/index.js
@@ -9,7 +9,7 @@ app.use(cors()); // Defaults to origin: '*'
 
 app.get('/', (req, res) => {
   console.log(`auth microservice`);
-  res.send('Hello World v17!');
+  res.send('Hello World v18!');
 });
 
 const register = new promClient.Registry();
